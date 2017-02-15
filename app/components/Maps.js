b57022d9a140d06f77f7ef3c33e591d386c69f52
Mathieu Débit <mathieu.debit@gmail.com>
2017-02-15 09:51:15 +0100

    Fix Google Maps on iOS

--- app/components/Maps.js
+++ app/components/Maps.js
@@ -7,14 +7,15 @@ class Maps extends Component {
   render() {
     return (
       <MapView
+        provider={MapView.PROVIDER_GOOGLE}
         style={styles.map}
+        customMapStyle={mapStyle}
         initialRegion={{
           latitude: 48.837118,
           longitude: 2.353756,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-        customMapStyle={mapStyle}
       />
     );
   }
