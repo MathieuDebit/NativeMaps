6038a3efc8bfacfbaa6581e600808912eba24809
Mathieu Débit <mathieu.debit@gmail.com>
2017-02-17 12:11:01 +0100

    Refactor Library modal component

--- app/components/Library.js
+++ app/components/Library.js
@@ -2,77 +2,88 @@ import React, { Component } from 'react';
 import { Modal, Text, TouchableHighlight, View, StyleSheet } from 'react-native';
 
 class Library extends Component {
+  constructor(props) {
+    super(props);
 
-  state = {
-    modalVisible: false,
-    isPressed: false
+    this.state = {
+      isModalOpen: false,
+      isPressed: false
+    }
+
+    this.openModalOnPress = this.openModalOnPress.bind(this);
+    this.openModalOnPressIn = this.openModalOnPressIn.bind(this);
+    this.openModalOnPressOut = this.openModalOnPressOut.bind(this);
+    this.closeModalOnPress = this.closeModalOnPress.bind(this);
   }
 
-  setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+  openModalOnPress() {
+
+  }
+
+  openModalOnPressIn() {
+    this.setState({ isPressed: true, isModalOpen: true })
+  }
+
+  openModalOnPressOut() {
+    this.setState({ isPressed: false })
+  }
+
+  modalOnRequestClose() {
+    alert("Modal has been closed.");
+  }
+
+  closeModalOnPress() {
+    this.setState({ isPressed: true, isModalOpen: false })
   }
 
   render() {
     return (
-      <View style={{marginTop: 22}}>
-        <Modal
-          animationType={"slide"}
-          transparent={false}
-          visible={this.state.modalVisible}
-          onRequestClose={() => {alert("Modal has been closed.")}}
-          >
-         <View style={{marginTop: 22}}>
-          <View>
-            <Text>Hello World!</Text>
-
-            <TouchableHighlight onPress={() => {
-              this.setModalVisible(!this.state.modalVisible)
-              this.setState({ isPressed: true })
-            }}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
-
-          </View>
-         </View>
-        </Modal>
-
+      <View style={styles.modalView}>
         <TouchableHighlight
-        style={ this.state.isPressed ? styles.showModalPressed : styles.showModal }
-        onPress={() => {
-          
-        }}
-        onPressIn={() => {
-          this.setState({ isPressed: true })
-          this.setModalVisible(true)
-        }}
-        onPressOut={() => {
-          this.setState({ isPressed: false })
-        }}
+          style={[
+            styles.openModalButton,
+            this.state.isPressed ? styles.openModalButtonPressed : styles.openModalButton
+          ]}
+          onPress={this.openModalOnPress}
+          onPressIn={this.openModalOnPressIn}
+          onPressOut={this.openModalOnPressOut}
         >
           <Text></Text>
         </TouchableHighlight>
 
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.isModalOpen}
+          onRequestClose={this.modalOnRequestClose}
+        >
+          <View style={styles.modalView}>
+            <Text>Library</Text>
+
+            <TouchableHighlight onPress={this.closeModalOnPress}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </Modal>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  showModal: {
-    backgroundColor: "#f1c40f",
+  modalView: {
+    marginTop: 22
+  },
+  openModalButton: {
     width: 40,
     height: 40,
+    marginBottom: 20,
     borderRadius: 50,
-    borderWidth: 2,
-    borderColor: '#f39c12',
-    overflow: 'hidden',
-    marginBottom: 20
+    overflow: "hidden",
+    backgroundColor: "orange",
   },
-  showModalPressed: {
-    backgroundColor: "#f39c12",
-    borderColor: '#f1c40f',
-    width: 45,
-    height: 45
+  openModalButtonPressed: {
+    backgroundColor: "tomato",
   }
 });
 
