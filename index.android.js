1fe97a46c439fe8c481faa2f802c762e608cf64a
Mathieu Débit <mathieu.debit@gmail.com>
2017-03-15 22:45:09 +0100

    Refactor folder structure

--- index.android.js
+++ index.android.js
@@ -1,11 +1,4 @@
-import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import App from './app/components/App';
+import App from './src/js/components/App';
 
-export default class NativeMaps extends Component {
-  render() {
-    return <App />;
-  }
-}
-
-AppRegistry.registerComponent('NativeMaps', () => NativeMaps);
+AppRegistry.registerComponent('NativeMaps', () => App);
